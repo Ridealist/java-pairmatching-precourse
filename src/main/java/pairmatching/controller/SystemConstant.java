6e4694af9780a094fe91691458b6deb5cd28d532
Ridealist <cjsn92@gmail.com>
2022-12-07 06:12:53 +0900

    feat(SystemConstant): 환경 변수 클래스 추가

---
+++ src/main/java/pairmatching/controller/SystemConstant.java
@@ -0,0 +1,11 @@
+package pairmatching.controller;
+
+public final class SystemConstant {
+
+    private SystemConstant() {}
+
+    public static final int COURSE_INDEX = 0;
+    public static final int LEVEL_INDEX = 1;
+    public static final int MISSION_INDEX = 2;
+    public static final String QUIT_REMATCHING_CHOICE = "아니오";
+}
